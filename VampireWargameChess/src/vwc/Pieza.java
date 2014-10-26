64932cafe19b0d90369a7e241d1d5ea6435473cb
Raim Interiano <rai.interiano@gmail.com>
2014-10-26 16:42:18 -0600

    Comentarios necesarios
    Comentarios para comprender la clase Pieza

--- VampireWargameChess/src/vwc/Pieza.java
+++ VampireWargameChess/src/vwc/Pieza.java
@@ -10,12 +10,36 @@ package vwc;
  * @author Raim
  */
 public abstract class Pieza {
-    protected String tipo;
-    protected int hp;
-    protected int sp;
-    protected int ap;
+    protected String tipo; //nombre del tipo de pieza
+    protected int row, column; //coordenadas de la posición de la pieza
+    protected int hp; //health points
+    protected int sp; //shield points
+    protected int ap; //attack points
     
-    public abstract Pieza atacar();
+    /**
+     * Realiza la acción necesaria de ataque
+     * @param p Objeto Pieza que se quiere atacar
+     */
+    public abstract void atacar(Pieza p);
+    
+    /**
+     * Valida la posición de la pieza
+     * @param fila Número de la fila de pieza
+     * @param columna Número de la columna de la pieza
+     * @return true si esa es la coordenada
+     */
+    public boolean validarPosicion(int fila, int columna){
+        if (fila == row && columna == column)
+            return true;
+        
+        return false;
+    }
+    
+    /**
+     * Mover la pieza a la coordenada especificada
+     * @param fila Número de la fila donde nos queremos mover
+     * @param columna Número de la columna donde nos queremos mover
+     */
     public abstract void mover (int fila, int columna);
     
     @Override
